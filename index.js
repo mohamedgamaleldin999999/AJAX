0912897593120a4b9d5a9c997d01a3ce1a2f367d
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-08 23:53:53 +0300

    Change parameter

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let url = 'https://jsonplaceholder.typicode.com/users';
 let xhr = XMLHttpRequest();
 xhr.open('GET', url, true);
-xhr.onreadystatechange = function(res) {
+
+xhr.onreadystatechange = function(ev) {
   console.log(xhr.readystate)
 };
 
 xhr.onerror = function(err) {
   console.warn(err)
 };
+
+xhr.send(null);
