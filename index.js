eb188afd37942d544e1746af9db4b5694345f343
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-09 00:08:01 +0300

    Add mor js

--- index.js
+++ index.js
@@ -3,7 +3,33 @@ let xhr = new XMLHttpRequest();
 xhr.open('GET', url, true);
 
 xhr.onreadystatechange = function(ev) {
-  console.log(xhr.readyState)
+  console.log(xhr.readyState);
+  if (xhr.readyState === 4) {
+    switch (xhr.status) {
+      case 200:
+      case 304:
+        console.log("OK or Not Modified (cached)", xhr.status);
+        outputUsers(xhr.responseText); //responseXML
+        break;
+      case 201:
+        console.log("Created", xhr.status);
+        let main = document.querySelector("main");
+        main.textContent = xhr.responseText;
+        break;
+      case 403:
+      case 401:
+        console.log("Not Authorized or Forbidden", xhr.status);
+        break;
+      case 404:
+        console.log("Not Found", xhr.status);
+        break;
+      case 500:
+        console.log("Server Side Error", xhr.status);
+        break;
+      default:
+        console.log("Some other code: ", xhr.status, xhr.status);
+    }
+  }
 };
 
 xhr.onerror = function(err) {
